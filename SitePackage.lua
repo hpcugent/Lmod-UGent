505edcb056fb484dd5ca575ae4097eb685e065e6
Ward Poelmans <wpoely86@gmail.com>
2016-02-18 13:57:06 +0100

    Update logging of a module load

--- SitePackage.lua
+++ SitePackage.lua
@@ -33,34 +33,25 @@
 --------------------------------------------------------------------------
 
 require("strict")
+local Dbg = require("Dbg")
+local dbg = Dbg:dbg()
 local hook   = require("Hook")
 
 -- By using the hook.register function, this function "load_hook" is called
 -- ever time a module is loaded with the file name and the module name.
-
-
 function load_hook(t)
    -- the arg t is a table:
    --     t.modFullName:  the module full name: (i.e: gcc/4.7.2)
    --     t.fn:           The file name: (i.e /apps/modulefiles/Core/gcc/4.7.2.lua)
 
-   -- Your site can use this any way that suits.  Here are some possibilities:
-   --  a) Write this information into a file in your users directory (say ~/.lmod.d/.save).
-   --     Then once a day/week/month collect this data.
-   --  b) have this function call syslogd to register that this module was loaded by this
-   --     user
-   --  c) Write the same information directly to some database.
-
-   -- This is an example writing to syslogd:
-
    if (mode() ~= "load") then return end
    local user  = os.getenv("USER")
-   local jobid = os.getenv("PBS_JOBID") or "unknown"
-   local msg   = string.format("user=%s,module=%s,fn=%s,job=%s",
-                               user, t.modFullName, t.fn, jobid)
-   os.execute("logger -t lmod -p local0.info " .. msg)
+   local jobid = os.getenv("PBS_JOBID")
+   local institute_cluster = os.getenv("VSC_INSTITUTE_CLUSTER")
+   local arch_local = os.getenv("VSC_ARCH_LOCAL")
+   local msg   = string.format("user=%s, cluster=%s, arch=%s, module=%s, fn=%s, jobid=%s",
+                               user, institute_cluster, arch_local, t.modFullName, t.fn, jobid)
+   os.execute("logger -t lmod -p user.notice " .. msg)
 end
 
 hook.register("load",load_hook)
-
-
