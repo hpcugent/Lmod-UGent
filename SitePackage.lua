3841a264e56029c75e4a9f67babb2371bf3a4e03
Ward Poelmans <wpoely86@gmail.com>
2016-05-25 12:03:48 +0200

    Add logging of all Lmod commands

--- SitePackage.lua
+++ SitePackage.lua
@@ -26,7 +26,7 @@ local function load_hook(t)
    local arch = os.getenv("VSC_ARCH_LOCAL") or ""
    local msg = string.format("user=%s, cluster=%s, arch=%s, module=%s, fn=%s, jobid=%s",
                              user, cluster, arch, t.modFullName, t.fn, jobid)
-   os.execute("logger -t lmod -p user.notice " .. msg)
+   os.execute("logger -t lmod -p user.notice -- " .. msg)
 end
 
 -- This hook is called after a restore operation
@@ -55,11 +55,21 @@ end
 local function startup_hook(usrCmd)
     -- usrCmd holds the currect active command
    -- if you want access to all give arguments, use
-   -- local masterTbl = masterTbl()
+   local masterTbl = masterTbl()
 
    dbg.start{"startup_hook"}
 
    dbg.print{"Received usrCmd: ", usrCmd, "\n"}
+
+   local cmd = usrCmd .. " " .. table.concat(masterTbl.pargs)
+   local user = os.getenv("USER")
+   local jobid = os.getenv("PBS_JOBID") or ""
+   local cluster = os.getenv("VSC_INSTITUTE_CLUSTER") or ""
+   local arch = os.getenv("VSC_ARCH_LOCAL") or ""
+   local msg = string.format("user=%s, cluster=%s, arch=%s, jobid=%s, cmd=%s",
+                             user, cluster, arch, jobid, cmd)
+   os.execute("logger -t lmod -p user.notice -- " .. msg)
+
    local ld_library_path = os.getenv("ORIG_LD_LIBRARY_PATH") or ""
    if ld_library_path ~= "" then
        dbg.print{"Setting LD_LIBRARY_PATH to ", ld_library_path, "\n"}
